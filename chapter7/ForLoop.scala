1a227e8963b518c3d007d7b60f2ca99adb866569
stolaine <sheshank238mukul@gmail.com>
2023-01-10 14:10:38 +0530

    Complex yield in for loop

--- chapter7/ForLoop.scala
+++ chapter7/ForLoop.scala
@@ -15,4 +15,17 @@ class ForLoop(filesHere: Array[File]) {
 
     def fileLines(file: File) = 
         scala.io.Source.fromFile(file).getLines().toList
+    
+    def getFileLineLength(pattern: String) = {
+        for {
+            file <- files
+            if file.getName.endsWith(".scala")
+            line <- fileLines(file)
+            trimmed = line.trim
+            if trimmed.matches(pattern)
+        } yield {
+            Console println file
+            trimmed.length
+        }
+    }
 }
--- chapter7/Run.scala
+++ chapter7/Run.scala
@@ -1,23 +1,27 @@
 object Run extends App {
     val filesHere = new java.io.File(".").listFiles
-    for (file <- filesHere
-        if file.isFile
-        if file.getName.endsWith(".scala"))
-        Console println file
+    // for (file <- filesHere
+    //     if file.isFile
+    //     if file.getName.endsWith(".scala"))
+    //     Console println file
     
-    for (i <- 1 to 4)
-        Console println s"Iteration $i"
+    // for (i <- 1 to 4)
+    //     Console println s"Iteration $i"
     
-    for (i <- 1 until 4)
-        Console println s"Iteration $i"
+    // for (i <- 1 until 4)
+    //     Console println s"Iteration $i"
     
     val obj = new ForLoop(filesHere)
-    obj.grep(".*gcd.*")
+    // obj.grep(".*gcd.*")
 
-    def scalaFiles = for (file <- filesHere
-        if file.getName.endsWith(".scala"))
-        yield file
+    // def scalaFiles = for (file <- filesHere
+    //     if file.getName.endsWith(".scala"))
+    //     yield file
     
-    for (file <- scalaFiles)
-        Console println file
+    // for (file <- scalaFiles)
+    //     Console println file
+    
+    val fileLineLengths = obj.getFileLineLength(".*for.*")
+    for (length <- fileLineLengths)
+        Console println length
 }
