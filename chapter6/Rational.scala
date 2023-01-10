3c512b506d57561c13ee9c6de655b7ff9113338e
stolaine <sheshank238mukul@gmail.com>
2023-01-10 08:37:46 +0530

    Private field and method

--- chapter6/Rational.scala
+++ chapter6/Rational.scala
@@ -1,11 +1,12 @@
 class Rational(n: Int, d: Int) {
     require(d!=0)
-    val numer: Int = n
-    val denom: Int = d
+    private val g = gcd(n.abs, d.abs)
+    val numer: Int = n/g
+    val denom: Int = d/g
 
     def this(n: Int) = this(n, 1)
     
-    override def toString = s"$n/$d"
+    override def toString = s"$numer/$denom"
 
     def add(that: Rational): Rational =
         new Rational(
@@ -18,4 +19,7 @@ class Rational(n: Int, d: Int) {
 
     def max(that: Rational): Rational =
         if (this lessThan that) that else this
+    
+    private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
 }
--- chapter6/Run.scala
+++ chapter6/Run.scala
@@ -1,8 +1,10 @@
 object Run extends App {
-    val oneHalf = new Rational(4, 2)
-    val oneThird = new Rational(1, 3)
+    // val oneHalf = new Rational(4, 2)
+    // val oneThird = new Rational(1, 3)
     // val sum = oneHalf add oneThird
-    val five = new Rational(5)
-    Console println five
+    // val five = new Rational(5)
+    // Console println five
     // Console println (oneHalf max oneThird)
+    val elevenBySeven = new Rational(66, 42)
+    Console println elevenBySeven
 }
